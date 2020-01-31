e4528d76aa97b02f22a988b09bc91e88692973ff
Andrew Dao <Andrew@Andrews-MBP-3.attlocal.net>
2020-01-30 18:24:51 -0800

    format

--- src/Practice01MathIterative.java
+++ src/Practice01MathIterative.java
@@ -1,37 +1,39 @@
 /**
- * this file only does math things iteratively 
+ * this file only does math things iteratively
  */
-public class Practice01MathIterative implements Practice01Math{
-public int fact(int n) throws Exception{
-    //if n is a negative number throw an exception
-    if(n<0){
-    throw new Exception();
-       }
-    
-    int x=1;
-    for(int i=1;i<=n;i++){
-        x=x*i; // x is updated every time the loop is going,starts at 1 *1 than stops at n(input)
-    }//end for
-    return x;
-}
-public int fib(int n) throws Exception{
-    //if n is a negative number throw an exception
-    if(n<0){
-        throw new Exception();
+public class Practice01MathIterative implements Practice01Math {
+    public int fact(int n) throws Exception {
+        // if n is a negative number throw an exception
+        if (n < 0) {
+            throw new Exception();
+        }
+
+        int x = 1;
+        for (int i = 1; i <= n; i++) {
+            x = x * i; // x is updated every time the loop is going,starts at 1 *1 than stops at
+                       // n(input)
+        } // end for
+        return x;
     }
-    //if n is one return 1
-    if(n <= 1) {
-        return n;
-    }
-    
-    int fib = 1;
-    int prevFib = 1;
-    
-    for(int i=2; i<n; i++) {
-        int temp = fib;
-        fib+= prevFib;
-        prevFib = temp;
-    }//end for
-    return fib;
-}//found on geeks for geeks
+
+    public int fib(int n) throws Exception {
+        // if n is a negative number throw an exception
+        if (n < 0) {
+            throw new Exception();
+        }
+        // if n is one return 1
+        if (n <= 1) {
+            return n;
+        }
+
+        int fib = 1;
+        int prevFib = 1;
+
+        for (int i = 2; i < n; i++) {
+            int temp = fib;
+            fib += prevFib;
+            prevFib = temp;
+        } // end for
+        return fib;
+    }// found on geeks for geeks
 }
--- src/Practice01MathRecursive.java
+++ src/Practice01MathRecursive.java
@@ -1,32 +1,31 @@
 /**
- * This program does math recursively only. 
+ * This program does math recursively only.
  */
 
-public class Practice01MathRecursive implements Practice01Math{
-    public int fact(int n) throws Exception{
-        //if n is negative throw exception
+public class Practice01MathRecursive implements Practice01Math {
+    public int fact(int n) throws Exception {
+        // if n is negative throw exception
         if (n < 0) {
             throw new Exception();
         }
-        
-        if(n<=1 || n==0)
+
+        if (n <= 1 || n == 0)
             return 1;
-        //for all else
+        // for all else
         else
-            return(n*fact(n-1));
+            return (n * fact(n - 1));
     }
 
-
-    public int fib(int n) throws Exception{
-        //if n is negative throw exception
-        if (n<0){
+    public int fib(int n) throws Exception {
+        // if n is negative throw exception
+        if (n < 0) {
             throw new Exception();
         }
 
-        if (n <= 1) 
-           return n; 
-        //for all else
-        return fib(n-1) + fib(n-2); 
+        if (n <= 1)
+            return n;
+        // for all else
+        return fib(n - 1) + fib(n - 2);
     }
 
-    }
+}
